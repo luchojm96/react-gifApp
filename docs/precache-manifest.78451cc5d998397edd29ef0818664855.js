debf19752ef3ef4cd10669ff7edf8cc6a6d7601e
Luis Jaldin <luchojm96@gmail.com>
2020-09-13 17:46:33 -0400

    Docs agregado

---
+++ docs/precache-manifest.78451cc5d998397edd29ef0818664855.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e66900e796a8b8b4dde2765c477e72b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a4efbacfadc43d6850c",
+    "url": "/static/css/main.b3f4fe33.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a4efbacfadc43d6850c",
+    "url": "/static/js/main.72911356.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
